cf1f55a3acc6e30567ee4684ef2578d014786d97
Jaeyoon <wodbs4338@naver.com>
2021-05-22 23:06:25 +0900

    StringCalculator 정규 표현식 사용 및 toNumArray 메소드 추가하여 코드 리팩토링

--- src/main/java/com/javaweb/nextstep/chap02/calculator/StringCalculator.java
+++ src/main/java/com/javaweb/nextstep/chap02/calculator/StringCalculator.java
@@ -1,33 +1,41 @@
 package com.javaweb.nextstep.chap02.calculator;
 
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    public int[] getSplitNums(String exp) {
-        boolean hasCustomDelimiter = exp.startsWith("//");
-        String delimiter;
+    public int[] toNumArray(String[] strArr) {
+        int[] nums = new int[strArr.length];
 
-        if(hasCustomDelimiter) {
-            delimiter = String.valueOf(exp.charAt(2));
-            exp = exp.substring(4);
-        } else {
-            delimiter = ";|,";
-        }
-
-        String[] arr = exp.split(delimiter);
-        int[] result = new int[arr.length];
-
-        for(int i=0; i<arr.length; i++) {
-            int num = Integer.parseInt(arr[i]);
+        for(int i=0; i<strArr.length; i++) {
+            int num = Integer.parseInt(strArr[i]);
 
             if(num < 0) {
                 throw new RuntimeException("입력값으로 음수가 들어올 수 없습니다.");
             }
 
-            result[i] = num;
+            nums[i] = num;
         }
+
+        return nums;
+    }
+
+    public int[] getSplitNums(String exp) {
+        boolean hasCustomDelimiter = exp.startsWith("//");
+        String delimiter = ";|,";
+
+        if(hasCustomDelimiter) {
+            Pattern pattern = Pattern.compile("//(.)\n(.*)");
+            Matcher matcher = pattern.matcher(exp);
+
+            if(matcher.find()) {
+                delimiter = matcher.group(1);
+                exp = matcher.group(2);
+            }
+        }
+
+        String[] arr = exp.split(delimiter);
+        int[] result = toNumArray(arr);
 
         return result;
     }
@@ -41,29 +49,5 @@ public class StringCalculator {
         }
 
         return sum;
-    }
-
-    public static void main(String[] args) {
-        String exp = "//,\n1,2,3";
-        String exp2= "1;2;3";
-        String t = "\\.";
-        String t2 = ".";
-        String t3;
-        t3 = t;
-
-        String[] arr = exp2.split(t);
-        String[] arr2 = exp2.split(t2);
-
-        new StringCalculator().getSplitNums(exp2);
-
-//        Pattern pattern;
-//        Matcher matcher;
-//        pattern = Pattern.compile("[0-9]");
-//        matcher = pattern.matcher(exp);
-//
-//        while(matcher.find()) {
-//            int idx =  matcher.start();
-//            System.out.println(exp.charAt(idx));
-//        }
     }
 }
